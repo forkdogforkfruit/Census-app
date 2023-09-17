a4f37d854e3a82e052d36f900654708491ba51c7
forkdogforkfruit <119700133+forkdogforkfruit@users.noreply.github.com>
2023-09-17 20:50:14 +0200

    Validation, handlers updated, deleted, etc

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var validator = require("validator")
 
 var indexRouter = require("./routes/index");
 var participantsRouter = require("./routes/participants");
--- routes/participants.js
+++ routes/participants.js
@@ -9,7 +9,6 @@ const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 let Validator = require("validatorjs");
-let validator = require("../middleware/validate");
 
 let rules = {
   email: "required|email",
@@ -18,8 +17,39 @@ let rules = {
   dob: "required|date",
   active: "required|boolean",
   companyname: "string",
+  salary: "integer",
+  currency: "integer",
+  country: "string",
+  city: "string",
 };
 const { requiresAuth } = require("express-openid-connect");
+
+async function getDetail(key) {
+  console.log("getDetail:", key);
+  let item = await participants.get(key);
+  let work = await participants.item(key).fragment("work").get();
+  let home = await participants.item(key).fragment("home").get();
+  let result = {
+    active: item.props.active,
+    lastName: item.props.lastName,
+    dob: item.props.dob,
+    created: item.props.created,
+    firstName: item.props.firstName,
+  };
+  if (work.length > 0)
+    result.work = {
+      companyname: work[0].props.companyname,
+      salary: work[0].props.salary,
+      currency: work[0].props.currency,
+    };
+  if (home.length > 0)
+    result.home = {
+      country: home[0].props.country,
+      city: home[0].props.city,
+    };
+  console.log("result", JSON.stringify(result));
+  return result;
+}
 
 /* GET a list of all participants. Shows only keys */
 //TODO: add requiresAuth
@@ -27,33 +57,43 @@ router.get("/", async function (req, res, next) {
   //  console.log(req.oidc.user);
   console.log("GET request for all participants made to /participants");
   let list = await participants.list();
-  res.send(list);
+  let result = list.results.map((element) => ({ email: element.key }));
+  res.send(result);
 });
 
-//TODO:get all details of all participants
+//get all details of all active participants
 router.get("/details", async function (req, res, next) {
   //console.log(req.oidc.user);
   let list = await participants.list();
-  let results = list.results[0];
-  //console.log(list);
-  res.send(results);
+  let listWithDetails = (
+    await Promise.all(
+      list.results.map(async (element) => await getDetail(element.key))
+    )
+  ).filter((element) => element.active);
+  res.send(listWithDetails);
 });
 
+// list inactive participants
 router.get("/details/deleted", async function (req, res, next) {
-  //TODO: need to show only inactive participants here.
+  let list = await participants.list();
+  let listWithDetails = (
+    await Promise.all(
+      list.results.map(async (element) => await getDetail(element.key))
+    )
+  ).filter((element) => !element.active);
+  res.send(listWithDetails);
 });
 
 //GET a specific record with use of a key (email)
 //TODO: add requiresAuth
 router.get("/details/:key", async function (req, res, next) {
   console.log("a record detail has been requested");
-  let item = await participants.get(req.params.key);
-  res.send(item);
+  res.send(await getDetail(req.params.key));
 });
 
 // POST a new record at /add
 //TODO: add requiresAuth
-router.post("/add", validator, async function (req, res, next) {
+router.post("/add", async function (req, res, next) {
   console.log("post request made to participants/add");
   const {
     email,
@@ -64,36 +104,35 @@ router.post("/add", validator, async function (req, res, next) {
     companyname,
     salary,
     currency,
+    country,
+    city,
   } = req.body;
 
-  let validation = new Validator(
-    req.body,
-    //{ email, firstName, lastName, dob, active },
-    rules
-  );
+  let validation = new Validator(req.body, rules);
   if (validation.passes()) {
-    await participants.set(
-      email,
-      {
-        firstName: firstName,
-        lastName: lastName,
-        dob: dob,
-        active: active,
-        //TODO: add fragments
-      }
-        .fragment("work")
-        .set({
-          companyname: companyname,
-          salary: salary,
-          currency: currency,
-        })
-    );
+    await participants.set(email, {
+      firstName: firstName,
+      lastName: lastName,
+      dob: dob,
+      active: active,
+    });
+    await participants.item(email).fragment("work").set({
+      companyname: companyname,
+      salary: salary,
+      currency: currency,
+    });
+    await participants.item(email).fragment("home").set({
+      country: country,
+      city: city,
+    });
+
     res.status(200).json({
       message: "Added record",
     });
   } else {
     res.status(400).json({
       message: "Incorrect information given",
+      errors: validation.errors.errors,
     });
   }
 });
@@ -102,7 +141,18 @@ router.post("/add", validator, async function (req, res, next) {
 //TODO: add requiresAuth
 router.put("/:email", async function (req, res, next) {
   console.log("put request made.");
-  const { email, firstName, lastName, dob, active } = req.body;
+  const {
+    email,
+    firstName,
+    lastName,
+    dob,
+    active,
+    companyname,
+    salary,
+    currency,
+    country,
+    city,
+  } = req.body;
 
   let validation = new Validator(
     req.body,
@@ -115,7 +165,15 @@ router.put("/:email", async function (req, res, next) {
       lastName: lastName,
       dob: dob,
       active: active,
-      //TODO add fragments
+    });
+    await participants.item(email).fragment("work").set({
+      companyname: companyname,
+      salary: salary,
+      currency: currency,
+    });
+    await participants.item(email).fragment("home").set({
+      country: country,
+      city: city,
     });
     res.status(200).json({
       message: "Record updated",
@@ -123,16 +181,20 @@ router.put("/:email", async function (req, res, next) {
   } else {
     res.status(400).json({
       message: "Failed to update record",
+      errors: validation.errors.errors,
     });
   }
 });
 
 //DELETES a record
-//TODO: add requiresAuth
-//TODO: change so that does not delete but changes from active to inactive
+//TODO: add requiresAuth`
 router.delete("/details/:email", async function (req, res, next) {
-  console.log(" A delete request has been made.");
-  await participants.delete(req.params.email);
+  console.log("A delete request has been made.");
+  const { email } = req.params;
+  console.log("email: ", email);
+  await participants.set(email, {
+    active: false,
+  });
   res.status(200).json({
     message: "Record deleted",
   });
