c361191d40b0665e2b8388fe9d6759589ae1ed55
forkdogforkfruit <119700133+forkdogforkfruit@users.noreply.github.com>
2024-03-18 15:49:43 +0100

    commented out auth

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("index", {
     title: "Express",
-    isAuthenticated: req.oidc.isAuthenticated(),
+    //isAuthenticated: req.oidc.isAuthenticated(),
   });
 });
 
